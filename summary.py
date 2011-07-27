9f59625dc4700bdd94b3ea1da6f14ca77aca8fb4
Christer Nyfält <cjnyfalt@yahoo.co.uk>
2011-07-27 23:19:09 +0300

    Made summary.py more flexible with input.

--- summary.py
+++ summary.py
@@ -4,13 +4,26 @@
 # Should later be made able to handle multiple files
 
 import sys
+import string
 from xml.dom import minidom
 from mech import *
 from armor import *
 from engine import *
 
+# If first argument is -f, read input list from file,
+if sys.argv[1] == "-f":
+    in_file = sys.argv[2]
+    f = open(in_file)
+    file_list_raw = f.readlines()
+    f.close()
+    # Strip out trailing newlines
+    file_list = map(string.strip, file_list_raw)
+# otherwise read in each argument as a mech file
+else:
+    file_list = sys.argv[1:]
+
 # Loop over input
-for i in sys.argv[1:]:
+for i in file_list:
     # Read file
     fsock = open(i)
     xmldoc = minidom.parse(fsock)
@@ -22,4 +35,4 @@ for i in sys.argv[1:]:
     name_str = mech.name + " " + mech.model
     move = mech.engine.get_move_string()
     armor = mech.armor.get_armor_percent()
-    print ("%-22s %3s %s %s" % (name_str, mech.weight, move, armor))
+    print ("%-26s %3s %-11s %4s" % (name_str, mech.weight, move, armor))
