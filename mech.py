8043644de2a3f63a258f0b95c8c49963bd6fd540
Christer Nyfält <cjnyfalt@yahoo.co.uk>
2011-08-06 19:49:18 +0300

    Improved weight_summary().

--- mech.py
+++ mech.py
@@ -210,10 +210,13 @@ class Mech:
         motive += self.engine.get_gyro_weight()
         motive += self.engine.get_jj_weight(self.weight)
         motive += self.engine.get_enh_weight()
+        motive += self.cockpit.get_cockpit_weight()
+        mratio = float(motive) / float(self.weight) * 100
         # defensive stuff
         defensive = self.structure.get_structure_weight()
         defensive += self.armor.get_armor_weight()
-        print "Total weight    : ", self.weight
-        print "Motive weight   : ", motive
-        print "Defensive weight: ", defensive
+        dratio = float(defensive) / float(self.weight) * 100
+        print ("Total weight    : %3.1ft" % (self.weight))
+        print ("Motive weight   : %3.1ft %2.1f%%" % (motive, mratio))
+        print ("Defensive weight: %3.1ft %2.1f%%" % (defensive, dratio))
         print "Other           : ", self.weight - motive - defensive
