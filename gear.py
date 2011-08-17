50dd19ef20202d5a9fec64fb744a7936cff27477
Christer Nyfält <cjnyfalt@yahoo.co.uk>
2011-08-17 19:56:49 +0300

    Really fix CASE bug.

--- gear.py
+++ gear.py
@@ -775,7 +775,7 @@ class Gear:
                     # we can use torso CASE
                     cas2 = self.case.get("LT", "")
                     # No CASE
-                    if ((cas != "CASE" and cas != "CASEII") or (cas2 != "CASE" and cas2 != "CASEII")):
+                    if ((cas != "CASE" and cas != "CASEII") and (cas2 != "CASE" and cas2 != "CASEII")):
                         neg_BV -= 15.0 * self.exp_ammo[i]
             elif (i == "RA"):
                 # Inner Sphere XL Engines means that side torsos are vulnerable
@@ -788,7 +788,7 @@ class Gear:
                     # we can use torso CASE
                     cas2 = self.case.get("RT", "")
                     # No CASE
-                    if ((cas != "CASE" and cas != "CASEII") or (cas2 != "CASE" and cas2 != "CASEII")):
+                    if ((cas != "CASE" and cas != "CASEII") and (cas2 != "CASE" and cas2 != "CASEII")):
                         neg_BV -= 15.0 * self.exp_ammo[i]
 
         return neg_BV
