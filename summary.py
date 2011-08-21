4526e6f88b7dfd786c7fcede2132c86716c85f0c
Christer Nyfält <cjnyfalt@yahoo.co.uk>
2011-08-21 16:46:32 +0300

    Output list of mechs with Narc.

--- summary.py
+++ summary.py
@@ -216,6 +216,45 @@ def print_C3i_list(file_list):
         print ("%-26s %4d %3d %.2f" % (i[0], i[1], i[2], i[3]))
 
 
+# Narc_list output
+#
+# In the form of name, BV, weight, BV/weight
+# sorted by BV/weight, descending
+#
+def print_Narc_list(file_list):
+    mech_list =[]
+    # Loop over input
+    for i in file_list:
+        # Load mech from file
+        mech = load_mech(i)
+
+        # Construct data
+        if mech.omni == "TRUE":
+            for i in mech.loads:
+                name_str = mech.name + " " + mech.model + i.name
+                BV = mech.get_BV(i)
+                weight = mech.weight
+                BV_t = float(BV)/float(weight)
+                if i.gear.has_narc:
+                    mech_list.append((name_str, BV, weight, BV_t))
+        else:
+            name_str = mech.name + " " + mech.model
+            BV = mech.get_BV(mech.load)
+            weight = mech.weight
+            BV_t = float(BV)/float(weight)
+            if mech.load.gear.has_narc:
+                mech_list.append((name_str, BV, weight, BV_t))
+
+    # Sort by BV/ton
+    mech_list.sort(key=itemgetter(3), reverse=True)
+
+    # Print output
+    print "Mechs with Narc:"
+    print "Name                       BV   Wgt BV/Wgt"
+    for i in mech_list:
+        print ("%-26s %4d %3d %.2f" % (i[0], i[1], i[2], i[3]))
+
+
 # Default output format, in flux
 def print_default(file_list):
     print "Name                       Wgt Movement    Armor  BV Mot   Def   Off"
@@ -283,6 +322,10 @@ for arg in sys.argv[1:]:
     elif arg == '-ci':
         output_type = 'ci'
         continue
+    # Narc summary output
+    elif arg == '-n':
+        output_type = 'n'
+        continue
     # otherwise read in each argument as a mech file
     else:
         file_list.append(''.join(arg))
@@ -300,6 +343,8 @@ elif output_type == 'cm':
     print_C3M_list(file_list)
 elif output_type == 'ci':
     print_C3i_list(file_list)
+elif output_type == 'n':
+    print_Narc_list(file_list)
 else:
     print_default(file_list)
 
