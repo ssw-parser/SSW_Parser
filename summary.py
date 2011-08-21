02f87e10d412ecf2a5e32e1e2906b16147909bd5
Christer Nyfält <cjnyfalt@yahoo.co.uk>
2011-08-21 15:23:03 +0300

    Moved default output of summary.py to own file.

--- summary.py
+++ summary.py
@@ -12,6 +12,10 @@ from defensive import *
 from movement import *
 
 # BV_list output
+#
+# In the form of name, BV, weight, BV/weight
+# sorted by BV/weight, descending
+#
 def print_BV_list(file_list):
     mech_list =[]
     # Loop over input
@@ -47,6 +51,39 @@ def print_BV_list(file_list):
     for i in mech_list:
         print ("%-26s %4d %3d %.2f" % (i[0], i[1], i[2], i[3]))
 
+
+# Default output format, in flux
+def print_default(file_list):
+    print "Name                       Wgt Movement    Armor  BV Mot   Def   Off"
+    # Loop over input
+    for i in file_list:
+        # Read file
+        fsock = open(i)
+        xmldoc = minidom.parse(fsock)
+        fsock.close()
+
+        # Get mech
+        mech = Mech(xmldoc)
+
+        name_str = mech.name + " " + mech.model
+        move = mech.get_move_string()
+        armor = mech.armor.get_armor_percent()
+    #    percent = mech.weight_summary(True)
+        if mech.omni == "TRUE":
+            for i in mech.loads:
+                BV = mech.get_BV(i)
+                name_str2 = name_str + i.name
+                print ("%-26s %3s %-11s %4s %4s" % (name_str2, mech.weight, move, armor, BV))
+        else:
+            BV = mech.get_BV(mech.load)
+            print ("%-26s %3s %-11s %4s %4s" % (name_str, mech.weight, move, armor, BV))
+#        print ("%-26s %3s %-11s %4s %4s %s" % (name_str, mech.weight, move, armor, BV, percent))
+
+
+####################################
+##### Main program starts here #####
+####################################
+
 ### Handle arguments ###
 file_list = []
 file_arg = False
@@ -80,28 +117,6 @@ for arg in sys.argv[1:]:
 
 if output_type == 'b':
     print_BV_list(file_list)
+else:
+    print_default(file_list)
 
-print "Name                       Wgt Movement    Armor  BV Mot   Def   Off"
-# Loop over input
-for i in file_list:
-    # Read file
-    fsock = open(i)
-    xmldoc = minidom.parse(fsock)
-    fsock.close()
-
-    # Get mech
-    mech = Mech(xmldoc)
-
-    name_str = mech.name + " " + mech.model
-    move = mech.get_move_string()
-    armor = mech.armor.get_armor_percent()
-#    percent = mech.weight_summary(True)
-    if mech.omni == "TRUE":
-        for i in mech.loads:
-            BV = mech.get_BV(i)
-            name_str2 = name_str + i.name
-            print ("%-26s %3s %-11s %4s %4s" % (name_str2, mech.weight, move, armor, BV))
-    else:
-        BV = mech.get_BV(mech.load)
-        print ("%-26s %3s %-11s %4s %4s" % (name_str, mech.weight, move, armor, BV))
-#        print ("%-26s %3s %-11s %4s %4s %s" % (name_str, mech.weight, move, armor, BV, percent))
