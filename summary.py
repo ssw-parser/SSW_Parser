d56ce4e23207171749078da568bd9f581be5d9f6
Christer Nyfält <cjnyfalt@yahoo.co.uk>
2011-09-05 01:35:33 +0300

    Added GPL license.

--- summary.py
+++ summary.py
@@ -3,6 +3,23 @@
 # Prints out a one-line summary of a mech
 # Should later be made able to handle multiple files
 
+#    SSW-file parser: Prints mech summaries
+#    Copyright (C) 2011  Christer Nyfält
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 import sys
 import string
 from xml.dom import minidom
