c759acae8a6ba2390dddc323441242aba1f43338
Christer Nyfält <cjnyfalt@yahoo.co.uk>
2012-01-26 15:16:42 +0200

    Removed year data from equipment tables.

--- gear.py
+++ gear.py
@@ -221,52 +221,52 @@ AMMO = {
 
 # Equipment, spilt into offensive, and defensive
 #
-# Name, BV, year, uses ammo rate, weight, explosive slots
+# Name, BV, uses ammo rate, weight, explosive slots
 #
 O_EQUIPMENT = {
-    "C3 Computer (Slave)" : [[0, 0], 3050, 0, 1, 0],
-    "C3 Computer (Master)" : [[0, 0], 3050, 0, 5, 0],
-    "Improved C3 Computer" : [[0, 0], 3062, 0, 2.5, 0],
-    "TAG" : [[0, 0], 2600, 0, 1, 0],
-    "Light TAG" : [[0, 0], 3054, 0, 0.5, 0],
-    "Cargo, Liquid" : [[0, 0], 1900, 0, 1, 0],
-    "Communications Equipment" : [[0, 0], 1900, 0, 1, 0],
-    "Remote Sensor Dispenser" : [[0, 0], 1900, 1, 0.5, 0],
+    "C3 Computer (Slave)" : [[0, 0], 0, 1, 0],
+    "C3 Computer (Master)" : [[0, 0], 0, 5, 0],
+    "Improved C3 Computer" : [[0, 0], 0, 2.5, 0],
+    "TAG" : [[0, 0], 0, 1, 0],
+    "Light TAG" : [[0, 0], 0, 0.5, 0],
+    "Cargo, Liquid" : [[0, 0], 0, 1, 0],
+    "Communications Equipment" : [[0, 0], 0, 1, 0],
+    "Remote Sensor Dispenser" : [[0, 0], 1, 0.5, 0],
     # Experimental
-    "Collapsible Command Module (CCM)" : [[0, 0], 2710, 0, 16, 0],
-    "Coolant Pod" : [[0, 0], 3049, 0, 1, 1]
+    "Collapsible Command Module (CCM)" : [[0, 0], 0, 16, 0],
+    "Coolant Pod" : [[0, 0], 0, 1, 1]
     }
 
 
 D_EQUIPMENT = {
-    "A-Pod" : [[1, 0], 3055, 0, 0.5, 0],
-    "B-Pod" : [[2, 0], 3069, 0, 1, 0],
-    "(IS) Anti-Missile System" : [[32, 11], 2617, 1, 0.5, 0],
-    "Guardian ECM Suite" : [[61, 0], 2597, 0, 1.5, 0],
-    "Beagle Active Probe" : [[10, 0], 2576, 0, 1.5, 0],
-    "ECM Suite" : [[61, 0], 2597, 0, 1, 0], # Clan
-    "Active Probe" : [[12, 0], 2576, 0, 1, 0], # Clan
-    "Light Active Probe" : [[7, 0], 2576, 0, 0.5, 0], # No year found
-    "(CL) Anti-Missile System" : [[32, 22], 2617, 1, 0.5, 0],
-    "CASE" : [[0, 0], 2476, 0, 0.5, 0], # HACK: CASE
+    "A-Pod" : [[1, 0], 0, 0.5, 0],
+    "B-Pod" : [[2, 0], 0, 1, 0],
+    "(IS) Anti-Missile System" : [[32, 11], 1, 0.5, 0],
+    "Guardian ECM Suite" : [[61, 0], 0, 1.5, 0],
+    "Beagle Active Probe" : [[10, 0], 0, 1.5, 0],
+    "ECM Suite" : [[61, 0], 0, 1, 0], # Clan
+    "Active Probe" : [[12, 0], 0, 1, 0], # Clan
+    "Light Active Probe" : [[7, 0], 0, 0.5, 0],
+    "(CL) Anti-Missile System" : [[32, 22], 1, 0.5, 0],
+    "CASE" : [[0, 0], 0, 0.5, 0], # HACK: CASE
     # Experimental
-    "Angel ECM" : [[100, 0], 3057, 0, 2, 0],
-    "Bloodhound Active Probe" : [[25, 0], 3058, 0, 2, 0],
-    "Electronic Warfare Equipment" : [[39, 0], 3025, 0, 7.5, 0],
-    "(CL) Laser Anti-Missile System" : [[45, 0], 3048, 0, 1, 0],
-    "(IS) CASE II" : [[0, 0], 3064, 0, 1, 0],
-    "(CL) CASE II" : [[0, 0], 3062, 0, 0.5, 0]
+    "Angel ECM" : [[100, 0], 0, 2, 0],
+    "Bloodhound Active Probe" : [[25, 0], 0, 2, 0],
+    "Electronic Warfare Equipment" : [[39, 0], 0, 7.5, 0],
+    "(CL) Laser Anti-Missile System" : [[45, 0], 0, 1, 0],
+    "(IS) CASE II" : [[0, 0], 0, 1, 0],
+    "(CL) CASE II" : [[0, 0], 0, 0.5, 0]
     }
 
 D_PHYSICAL = {
-    "Small Shield" : [[50, 0], 3067, 0, 2, 0]
+    "Small Shield" : [[50, 0], 0, 2, 0]
     }
 
 
 
 # Equipment, not split
 #
-# Name : BV, rules level, year, weight, uses ammo rate, explosive slots
+# Name : BV, rules level, weight, uses ammo rate, explosive slots
 #
 # Where rules level is: 0 = intro, 1 = tournament legal, 2 = advanced,
 # 3 = experimental, 4 = primitive
@@ -274,35 +274,35 @@ D_PHYSICAL = {
 # Note that a different order is used compared to the other tables
 #
 EQUIPMENT = {
-    "C3 Computer (Slave)" : [[0, 0], 1, 3050, 1, 0, 0],
-    "C3 Computer (Master)" : [[0, 0], 1, 3050, 5, 0, 0],
-    "Improved C3 Computer" : [[0, 0], 1, 3062, 2.5, 0, 0],
-    "TAG" : [[0, 0], 1, 2600, 1, 0, 0],
-    "Light TAG" : [[0, 0], 1, 3054, 0.5, 0, 0],
-    "Cargo, Liquid" : [[0, 0], 1, 1900, 1, 0, 0],
-    "Communications Equipment" : [[0, 0], 1900, 0, 1, 0],
-    "Remote Sensor Dispenser" : [[0, 0], 1900, 1, 0.5, 0],
+    "C3 Computer (Slave)" : [[0, 0], 1, 1, 0, 0],
+    "C3 Computer (Master)" : [[0, 0], 1, 5, 0, 0],
+    "Improved C3 Computer" : [[0, 0], 1, 2.5, 0, 0],
+    "TAG" : [[0, 0], 1, 1, 0, 0],
+    "Light TAG" : [[0, 0], 1, 0.5, 0, 0],
+    "Cargo, Liquid" : [[0, 0], 1, 1, 0, 0],
+    "Communications Equipment" : [[0, 0], 0, 1, 0],
+    "Remote Sensor Dispenser" : [[0, 0], 1, 0.5, 0],
     # Experimental
-    "Collapsible Command Module (CCM)" : [[0, 0], 3, 2710, 16, 0, 0],
-    "Coolant Pod" : [[0, 0], 2, 3079, 1, 0, 1],
+    "Collapsible Command Module (CCM)" : [[0, 0], 3, 16, 0, 0],
+    "Coolant Pod" : [[0, 0], 2, 1, 0, 1],
 
-    "A-Pod" : [[1, 0], 1, 3055, 0.5, 0, 0],
-    "B-Pod" : [[2, 0], 1, 3069, 1, 0, 0],
-    "(IS) Anti-Missile System" : [[32, 11], 1, 2617, 0.5, 1, 0],
-    "Guardian ECM Suite" : [[61, 0], 1, 2597, 1.5, 0, 0],
-    "Beagle Active Probe" : [[10, 0], 1, 2576, 1.5, 0, 0],
-    "ECM Suite" : [[61, 0], 1, 2597, 1, 0, 0], # Clan
-    "Active Probe" : [[12, 0], 1, 2576, 1, 0, 0], # Clan
-    "Light Active Probe" : [[7, 0], 1, 2576, 0.5, 0, 0], # No year found
-    "(CL) Anti-Missile System" : [[32, 22], 1, 2617, 0.5, 1, 0],
-    "CASE" : [[0, 0], 1, 2476, 0.5, 0, 0], # HACK: CASE
+    "A-Pod" : [[1, 0], 1, 0.5, 0, 0],
+    "B-Pod" : [[2, 0], 1, 1, 0, 0],
+    "(IS) Anti-Missile System" : [[32, 11], 1, 0.5, 1, 0],
+    "Guardian ECM Suite" : [[61, 0], 1, 1.5, 0, 0],
+    "Beagle Active Probe" : [[10, 0], 1, 1.5, 0, 0],
+    "ECM Suite" : [[61, 0], 1, 1, 0, 0], # Clan
+    "Active Probe" : [[12, 0], 1, 1, 0, 0], # Clan
+    "Light Active Probe" : [[7, 0], 1, 0.5, 0, 0],
+    "(CL) Anti-Missile System" : [[32, 22], 1, 0.5, 1, 0],
+    "CASE" : [[0, 0], 1, 0.5, 0, 0], # HACK: CASE
     # Experimental
-    "Angel ECM" : [[100, 0], 2, 3080, 2, 0, 0],
-    "Bloodhound Active Probe" : [[25, 0], 2, 3082, 2, 0, 0],
-    "Electronic Warfare Equipment" : [[39, 0], 3, 3025, 7.5, 0, 0],
-    "(CL) Laser Anti-Missile System" : [[45, 0], 2, 3079, 1, 0, 0],
-    "(IS) CASE II" : [[0, 0], 2, 3082, 1, 0, 0],
-    "(CL) CASE II" : [[0, 0], 2, 3082, 0.5, 0, 0]
+    "Angel ECM" : [[100, 0], 2, 2, 0, 0],
+    "Bloodhound Active Probe" : [[25, 0], 2, 2, 0, 0],
+    "Electronic Warfare Equipment" : [[39, 0], 3, 7.5, 0, 0],
+    "(CL) Laser Anti-Missile System" : [[45, 0], 2, 1, 0, 0],
+    "(IS) CASE II" : [[0, 0], 2, 1, 0, 0],
+    "(CL) CASE II" : [[0, 0], 2, 0.5, 0, 0]
     }
 
 # Targeting computers
@@ -442,7 +442,7 @@ class Equip(Item):
         # Physical parameter
         if (self.typ == "equipment" or self.typ == "CASE" or
             self.typ == "CASEII"):
-            return EQUIPMENT[self.name][3]
+            return EQUIPMENT[self.name][2]
         elif self.typ == "ammunition":
             return AMMO[self.name][2]
         elif self.typ == "physical":
@@ -507,9 +507,9 @@ class Equipment:
     def __init__(self, key, dic):
         self.name = key
         self.batt_val = dic[key][0]
-        self.useammo = dic[key][2]
-        self.weight = dic[key][3]
-        self.explosive = dic[key][4]
+        self.useammo = dic[key][1]
+        self.weight = dic[key][2]
+        self.explosive = dic[key][3]
         self.count = 0
         self.ammocount = 0
         self.ammo_ton = 0
