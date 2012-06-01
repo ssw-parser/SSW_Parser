8b6cfc6a43acef31870134ecfd8e9cc8cd524e42
Christer Nyfält <cjnyfalt@yahoo.co.uk>
2012-06-01 23:16:28 +0300

    Started working on setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name = "SSW Parser",
+      version = "0.0.1",
+      author = "Christer Nyfält",
+      author_email = "cjnyfalt@yahoo.co.uk",
+      description = "Outputs various reports on mechs made with SSW",
+      py_modules = ["ssw_parser"],
+      keywords = "SSW mechs analysis reports",
+      license = "GNU GENERAL PUBLIC LICENSE",
+)
